83c0bf9b2dd83c256ab6dccb2564feaea2b81c94
tanveer8482 <tanveer8482@gmail.com>
2026-05-06 03:51:38 -0700

    Use named Firestore database

--- shared/firebaseConfig.ts
+++ shared/firebaseConfig.ts
@@ -13,7 +13,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const db = getFirestore(app, "tanveer8482");
 export const storage = getStorage(app);
 export const auth = getAuth(app);
 
