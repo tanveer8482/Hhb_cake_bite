f279012bf7da1a9697f432fd04ac64d98fda8f9f
tanveer8482 <tanveer8482@gmail.com>
2026-05-08 08:01:31 -0700

    Set CORS to allow all origins and ensure POST handling

--- api/send-whatsapp.js
+++ api/send-whatsapp.js
@@ -17,7 +17,7 @@ const sanitizePakistanWhatsAppNumber = (phoneNumber = '') => {
 
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Allow', 'POST,OPTIONS');
